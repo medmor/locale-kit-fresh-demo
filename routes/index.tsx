6536085b4728af1694d57acb5bcf305dcafdb9e5
medmor <35731087+medmor@users.noreply.github.com>
2023-02-04 22:03:50 +0000

    lang routing

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,8 +3,7 @@ import { locale_kit } from "localekit";
 
 export const handler: Handlers = {
   async GET(req, _ctx) {
-    console.log(req);
-    const url = req.headers.get("x-forwarded-host")
+    const url = req.headers.get("x-forwarded-host") //check codesapace forwarding
       ? `https://${req.headers.get("x-forwarded-host")}/`
       : req.url;
     return await Response.redirect(url + locale_kit.fallback_language, 307);
